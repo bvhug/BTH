cc58134194e479ed0f70f9517749bcdc1b501dd3
bvhug <trvhung168@gmail.com>
2023-08-31 21:23:47 +0700

    HTML

---
+++ DemoGit/Program.cs
@@ -0,0 +1,15 @@
+using Internal;
+using System;
+using Microsoft.VisualBasic.CompilerServices;
+public class Program
+{
+    private static void public static void Main(string[] args)
+    {
+        int a,b;
+        System.Console.Write("a=5");
+        a = Convert.ToInt32(Console.ReadLine());
+        System.Console.Write("b=7");
+        b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("{0}+{1}={2},a,b,a+b")
+    }
+}
