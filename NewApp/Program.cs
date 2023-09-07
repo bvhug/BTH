a06dbe17f69c2d3e6ff4d803249299b714d05ef4
bvhug <trvhung168@gmail.com>
2023-09-07 17:51:55 +0700

    BTH2

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        String hoTen = "Bui Viet Hung";
-        int tuoi = 23;
-        String diachi = "Ha Noi";
-        int Msv = 1921050280;
+        const string hoTen = "Bui Viet Hung";
+        const int tuoi = 23;
+        const string diachi = "Ha Noi";
+        const int Msv = 1921050280;
         Console.WriteLine("Sinh vien:{0} - {1}tuoi",hoTen,tuoi);
         Console.WriteLine("Dia chi:{0} - MSV:{1}",diachi,Msv);
     }
