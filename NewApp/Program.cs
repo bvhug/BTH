e86c7c31bf0c281cfa1131d206f316872c25b004
bvhug <trvhung168@gmail.com>
2023-09-07 17:47:25 +0700

    BTH1

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program 
+{
+    private static void Main(string[] args)
+    {
+        String hoTen = "Bui Viet Hung";
+        int tuoi = 23;
+        String diachi = "Ha Noi";
+        int Msv = 1921050280;
+        Console.WriteLine("Sinh vien:{0} - {1}tuoi",hoTen,tuoi);
+        Console.WriteLine("Dia chi:{0} - MSV:{1}",diachi,Msv);
+    }
+}
