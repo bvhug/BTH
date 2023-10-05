ec6f4c41c04b6052a1e7b8e04c5c7d71fd3a59fb
bvhug <trvhung168@gmail.com>
2023-10-05 11:05:27 +0700

    BTH

--- NewApp/Models/Fruit.cs
+++ NewApp/Models/Fruit.cs
@@ -2,6 +2,25 @@ namespace NewApp.Models
  {
  public class Fruit {
         public string FruitName {get;set;}
+        public int weight {get; set;}
+       public void EnterData()
+       {
+        System.Console.Write("Fruit name = ");
+        FruitName = Console.ReadLine();
+        System.Console.Write("weight = ");
+        try{
+            weight = Convert.ToInt16(Console.ReadLine());
+        } catch(Exception e)
+        {
+            weight = 0;
+        }
+
+    }
+    public void Display()
+    {namespace NewApp.Models
+ {
+ public class Fruit {
+        public string FruitName {get;set;}
         public string Information {get; set;}
        
 
@@ -12,4 +31,8 @@ namespace NewApp.Models
             return TTien;
         }
  }
+ }
+        System.Console.WriteLine("{0} + {1} " , FruitName, weight);
+    }
+}
  }
