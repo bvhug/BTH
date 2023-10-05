bb4d6b663b9293c6e6d2457cce69094cd94ed86b
bvhug <trvhung168@gmail.com>
2023-10-05 11:24:19 +0700

    BTH

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,10 +1,10 @@
-﻿﻿using NewApp.Models;
-public class Program{   
-     public static void Main(String[] args){
-     Fruit frt = new Fruit();
-       string FruitName ="Lemon";
-       int b = 30000 ;
-       
-       Console.WriteLine("{0}  co thanh tien la: {1} ", FruitName, frt.ThanhTien(b));
-    }
-}
+﻿﻿	﻿using NewApp.Models;
+	public class Program{   
+		 public static void Main(String[] args){
+			  {
+			Person ps = new Person();
+			ps.EnterData();
+			ps.Display();
+		  }
+	}
+	}
