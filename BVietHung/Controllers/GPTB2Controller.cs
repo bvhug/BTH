01c1366050a5cafbf603be91d120e98b8d1b639d
bvhug <trvhung168@gmail.com>
2023-10-26 09:40:51 +0700

    GPTB2

---
+++ BVietHung/Controllers/GPTB2Controller.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+using DemoMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+//BuiVietHung-1921050280
+public class PTBacHaiController : Controller
+{
+    public IActionResult IndexPTBacHai()
+    {
+        return View();
+    }
+    [HttpPost]
+
+
+    public IActionResult IndexPTBacHai(GiaiPTBac2 model)
+    {
+        double delta = model.b * model.b - 4 * model.a * model.c;
+
+        if (delta < 0)
+        {
+            ViewData["Result"] = "Phuong trinh vo nghiem";
+        }
+        else if (delta == 0)
+        {
+            model.x1 = -model.b / (2 * model.a);
+            ViewData["Result"] = "Phuong trinh co nghiem kep: " + model.x1;
+        }
+        else
+        {
+            model.x1 = (-model.b + Math.Sqrt(delta)) / (2 * model.a);
+            model.x2 = (-model.b - Math.Sqrt(delta)) / (2 * model.a);
+            ViewData["Result"] = "Phuong trinh co 2 nghiem: x1 = " + model.x1 + ", x2 = " + model.x2;
+        }
+
+        return View("IndexPTBacHai", model);
+    }
+}
--- BVietHung/Controllers/StudentController.cs
+++ BVietHung/Controllers/StudentController.cs
@@ -1,7 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-
-namespace BVietHung.Controllers;
-public class StudentController : Controller 
+using DemoMVC.Models;
+namespace DemoMVC.Controllers
 {
+    public class StudentController:Controller
+{
+    public IActionResult Index()
+    {
+        return View();
 
+    }
+
+    [HttpPost]
+    public IActionResult Index(Student std)
+    {
+        string str = "hello " +  std.StudentID + " " + std.FullName;
+        ViewBag.ThongBao = str;
+        return View();
+    }
+//Bui Viet Hung -1921050280
+}
 }
