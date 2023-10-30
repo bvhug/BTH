6f5d51108ffdc618ca60f70fb768f3a52eef2688
bvhug <trvhung168@gmail.com>
2023-10-30 09:50:59 +0700

    Slide 20

---
+++ BVietHung/Models/Employee.cs
@@ -0,0 +1,9 @@
+namespace BVietHung.Models
+{
+    public class Employee : Person
+    {
+         public string EmployeeID  { get; set; }
+         public int Age  { get; set; }
+         public string SoDienThoai { get; set; }
+    }
+}
