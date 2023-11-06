2f19e2d620301547369d85dda0ccf0d62e97e9e9
bvhug <trvhung168@gmail.com>
2023-11-06 12:12:51 +0700

    Person

--- BVietHung/Models/Person.cs
+++ BVietHung/Models/Person.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace BVietHung.Models
 {
+    [Table("Person")]
     public class Person 
     {
+        [Key]
          public string PersonID { get; set; }
          public string FullName { get; set; }
          public string Address { get; set; }
