f4406220457ca4fa20e6a3f9bbf9e77a6ca28bf9
bvhug <trvhung168@gmail.com>
2023-11-09 17:39:55 +0700

    Slide 67

--- BVietHung/Data/ApplicationDbcontext.cs
+++ BVietHung/Data/ApplicationDbcontext.cs
@@ -5,8 +5,10 @@ namespace BVietHung.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
-        public DbSet<Person> Person {get ; set ;}
+        public DbSet<Person> Person { get; set;}
+        public DbSet<BVietHung.Models.Employee> Employee { get; set; } = default!;
+// khai báo việc ánh xạ class Person vào trong database
     }
 }
