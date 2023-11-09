1c8a9afc228b962cf7dbef889bc44ec85c6650d5
bvhug <trvhung168@gmail.com>
2023-11-09 17:38:44 +0700

    Sile 67

--- BVietHung/Migrations/20231109103502_Column_Person_Address.Designer.cs
+++ BVietHung/Migrations/20231109103502_Column_Person_Address.Designer.cs
@@ -2,6 +2,7 @@
 using BVietHung.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BVietHung.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231109103502_Column_Person_Address")]
+    partial class Column_Person_Address
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
---
+++ BVietHung/Migrations/20231109103502_Column_Person_Address.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BVietHung.Migrations
+{
+    /// <inheritdoc />
+    public partial class Column_Person_Address : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Person",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Person",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeID",
+                table: "Person",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SoDienThoai",
+                table: "Person",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeID",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "SoDienThoai",
+                table: "Person");
+        }
+    }
+}
