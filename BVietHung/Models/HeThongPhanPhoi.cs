3af209b6575609f79491873f1db1bd4c874b9c3f
bvhug <trvhung168@gmail.com>
2023-11-09 17:38:58 +0700

    Sile 67

--- BVietHung/Models/HeThongPhanPhoi.cs
+++ BVietHung/Models/HeThongPhanPhoi.cs
@@ -2,7 +2,7 @@ namespace BVietHung.Models
 {
     public class HeThongPhanPhoi
     {
-          public string MaHTPP { get; set; }
+         public string MaHTPP { get; set; }
          public string TenHTPP { get; set; }
     }
     //BuiVietHung-1921050280
--- BVietHung/Models/Person.cs
+++ BVietHung/Models/Person.cs
@@ -2,12 +2,13 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace BVietHung.Models
 {
-    [Table("Person")]
-    public class Person 
+   [Table("Person")]
+    public class Person
     {
         [Key]
-         public string PersonID { get; set; }
-         public string FullName { get; set; }
-         public string Address { get; set; }
+        public string PersonID { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
     }
 }
