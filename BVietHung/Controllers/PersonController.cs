d1b10b8c9510f2178ecd7851bdcfc401554496ba
bvhug <trvhung168@gmail.com>
2023-11-09 10:35:50 +0700

    CRUD_MVC

--- BVietHung/Controllers/PersonController.cs
+++ BVietHung/Controllers/PersonController.cs
@@ -5,9 +5,9 @@ using BVietHung.Data;
 using BVietHung.Models;
 namespace BVietHung.Controllers
 {
-    public class PersonController : Controller 
+    public class PersonController : Controller
     {
-       private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
         public PersonController(ApplicationDbContext context)
         {
             _context = context;
@@ -23,19 +23,101 @@ namespace BVietHung.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PersonID,FullName,Address")] Person person)
+
+         public async Task<IActionResult> Create([Bind("PersonID,FullName,Address")] Person person)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(p erson);
+                _context.Add(person);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(person);
         }
+            public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+            var person = await _context.Person.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+             public async Task<IActionResult> Edit(string id, [Bind("PersonID,FullName,Age,Address")] Person person)
+        {
+            if (id != person.PersonID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            //Bui Viet Hung -1921050280
+            return View(person);
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Person == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Person
+                .FirstOrDefaultAsync(m => m.PersonID == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Person == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Person'  is null.");
+            }
+            var person = await _context.Person.FindAsync(id);
+            if (person != null)
+            {
+                _context.Person.Remove(person);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         private bool PersonExists(string id)
         {
-            return (_context.Person?.Any(e => e.PersonID == id)).GetValueOrDefault();
+          return (_context.Person?.Any(e => e.PersonID == id)).GetValueOrDefault();
         }
+      //Bui Viet Hung -1921050280
     }
 }
